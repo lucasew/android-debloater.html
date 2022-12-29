24a37158642fb40f6cbdc854511e94bcafdcb9ef
lucasew <lucas59356@gmail.com>
2022-12-29 13:05:46 -0300

    checkpoint
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
-import { connection, setupConnection } from './lib/adb.ts';
+import { connection, setupConnection, packageList, adbState, adbStateString} from './lib/adb.ts';
 </script>
 
 <h1>Teste</h1>
 
+<p>Estado: {$adbStateString}</p>
+
+
 <button on:click={setupConnection}>Testar</button>
+
+{#await $packageList}
+    <p>Carregando lista de apps</p>
+{:then apps}
+    {#if apps.length == 0}
+        <p>Nenhum app encontrado (ainda)</p>
+    {:else}
+        {#each apps as app}
+            <section style="margin-top: 1rem;">
+                {#each Object.keys(app) as k}
+                    <p style="margin: 0;"><b>{k}</b> = {app[k]}</p>
+                {/each}
+            </section>
+        {/each}
+    {/if}
+{:catch error}
+    <p style="color: red">{error.message || error}</p>
+{/await}
--- src/lib/adb.ts
+++ src/lib/adb.ts
@@ -1,15 +1,86 @@
 import USBBackend from '@yume-chan/adb-backend-webusb';
-import { writable } from 'svelte/store';
+import { writable, derived, get } from 'svelte/store';
 import { Adb, AdbPublicKeyAuthenticator } from '@yume-chan/adb';
 import AdbWebCredentialStore from '@yume-chan/adb-credential-web';
+import { convert as axmlConvert } from 'axml2xml';
+import {Buffer} from 'buffer';
+import { join as shlexJoin } from 'shlex';
+Buffer.poolSize = 1;
+
+enum ConnectionState {
+  Unpaired,
+  Unauthenticated,
+  Ready
+}
+
+export const adbState = writable<ConnectionState>(ConnectionState.Unpaired);
+export const adbStateString = derived(adbState, (s) => ConnectionState[s])
+
+adbState.subscribe((s) => console.log('state', ConnectionState[s]))
 
 export const connection = writable<Adb|null>(null);
+connection.subscribe((s) => console.log('connection', s))
+
+export const packageList = derived(connection, async (adb) => {
+  if (!adb) {
+    return [];
+  }
+  const output = await adb.subprocess.spawnAndWaitLegacy([
+    "pm", "list", "packages",
+    "-f", // mostrar arquivo associado
+    "-e", // só apps ativados
+    "-i", // mostrar o instalador dos pacotes
+    "-U" // mostrar uuid
+  ]);
+  const encoder = new TextEncoder();
+  return (await Promise.all(output.split("\n").map(async item => {
+    let ret = {}
+    item.split(" ").map(part => {
+      let [k, v] = part.split(":");
+      if (!v) {
+        [k, v] = part.split("=");
+      }
+      if (k !== "") {
+        ret[k] = v;
+      }
+    })
+    if (!ret['package']) {
+      return null
+    }
+    const [packagePath, packageName] = ret['package'].split("=");
+    ret['package'] = packageName;
+    ret['apk'] = packagePath;
+    if (packageName === "") {
+      return null
+    }
+    const shlexCommand = ["unzip", packagePath, "AndroidManifest.xml", "-p"]
+    // const catAndroidManifest = ["sh", "-c", `${shlexCommand} | base64`]
+    // console.log(catAndroidManifest)
+    ret['manifest'] = await adb.subprocess.spawnAndWaitLegacy(shlexCommand)
+    console.log(ret);
+    // const buf = Buffer.from(encoder.encode(ret['manifest']).buffer)
+    const buf = Buffer.from(ret['manifest'], 'utf-8')
+    console.log(buf)
+    try {
+      ret['manifest'] = axmlConvert(buf);
+    } catch (e) {
+      console.log("Fail: decode manifest", ret['package'], e)
+    }
+    return ret
+  }))).filter(x => !!x)
+});
+
+packageList.subscribe((s) => console.log("apps", s))
+
 const credentialStore = new AdbWebCredentialStore();
 const authenticator = AdbPublicKeyAuthenticator();
 
-connection.subscribe(console.log)
-
+let setupOnce = false;
 export async function setupConnection() {
+  if (setupOnce) {
+    window.location.reload();
+  }
+  setupOnce = true;
   const device = await USBBackend.requestDevice();
   if (device === undefined) {
     // TODO: i18n
@@ -17,9 +88,11 @@ export async function setupConnection() {
     return
   }
   const conn = await device.connect();
+  adbState.set(ConnectionState.Unauthenticated)
   console.log('conn', conn);
   const session = await Adb.authenticate(conn, credentialStore);
-  console.log(await session.subprocess.spawnAndWaitLegacy(["getprop"]));
+  adbState.set(ConnectionState.Ready)
+  // console.log(await session.subprocess.spawnAndWaitLegacy(["pm", "list", "packages"]));
   connection.set(session);
 }
 
